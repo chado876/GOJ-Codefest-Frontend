2698f6ad16ce77c3928c2055e8155344a7efb0ef
Gabrielle Davidson <gabbydvdsn16@gmail.com>
2020-10-22 19:29:14 -0500

    Added views for admin dahboard

--- src/app/dashboard-currentmonth/dashboard-currentmonth.component.ts
+++ src/app/dashboard-currentmonth/dashboard-currentmonth.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as Chart from 'chart.js';
 
 @Component({
   selector: 'app-dashboard-currentmonth',
@@ -8,5 +9,37 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardCurrentmonthComponent implements OnInit {
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    var ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+        datasets: [
+          {
+            data: [15339, 21345, 18483, 24003, 23489, 24092, 12034],
+            lineTension: 0,
+            backgroundColor: 'transparent',
+            borderColor: '#007bff',
+            borderWidth: 4,
+            pointBackgroundColor: '#007bff',
+          },
+        ],
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: false,
+              },
+            },
+          ],
+        },
+        legend: {
+          display: false,
+        },
+      },
+    });
+  }
 }
